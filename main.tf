b306bc7a24034b1d5bc6b554cf2e9ca402cd03a1
Sahaja Undavalli <sahajaundavalli@sahajas-air.lan>
2023-05-02 22:38:44 -0400

    Associate subnets to route table

--- main.tf
+++ main.tf
@@ -30,3 +30,33 @@ resource "aws_subnet" "private_subnets" {
     Name = "Private Subnet ${count.index + 1}"
   }
 }
+
+#Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "Project VPC IG"
+  }
+}
+
+#route table
+resource "aws_route_table" "second_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "2nd Route Table"
+  }
+}
+
+#Associating Public Subnets to the Second Route Table,which has a route to the internet via the IGW
+resource "aws_route_table_association" "public_subnet_asso" {
+  count = length(var.public_subnet_cidrs)
+  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.second_rt.id
+}
